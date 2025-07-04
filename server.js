7a97287ca870c4ec85d0cc50e55b796ff974bdea
G Dhia <gmaulan47@gmail.com>
2025-07-04 20:39:41 +0700

    Add cache-busting headers to prevent browser caching
    - Added no-cache headers to index.html route
    - This prevents browsers from caching old versions of the UI
    - Fixes issue where admin 10 exclusion wasn't working due to cached HTML

--- server.js
+++ server.js
@@ -365,6 +365,12 @@ app.get('/logs/:jobId', (req, res) => {
 
 app.get('/', (req, res) => {
   console.log('Serving index.html');
+  // Add cache-busting headers
+  res.set({
+    'Cache-Control': 'no-cache, no-store, must-revalidate',
+    'Pragma': 'no-cache',
+    'Expires': '0'
+  });
   res.sendFile(__dirname + '/index.html');
 });
 
