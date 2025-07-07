d8a6d31c831b1651bc60c25344596a688f358ee0
G Dhia <gmaulan47@gmail.com>
2025-07-07 14:07:23 +0700

    add 0.0.0.0 binding

--- server.js
+++ server.js
@@ -498,6 +498,6 @@ app.post('/run', (req, res) => {
     });
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server started on http://localhost:${PORT}`);
 });
