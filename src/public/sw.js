fb17505d99bda724fcdc1a34989e915e517bec9a
gustimaulanid <gmaulan47@gmail.com>
2026-01-17 14:13:07 +0700

    fix: ensure app.js readable in prod 4

--- src/public/sw.js
+++ src/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'efe-admin-lo-v2';
+const CACHE_NAME = 'efe-admin-lo-v3';
 const urlsToCache = [
   '/',
   '/js/app.js',
@@ -26,24 +26,24 @@ self.addEventListener('fetch', event => {
         if (response) {
           return response;
         }
-        
+
         // Clone the request
         const fetchRequest = event.request.clone();
-        
+
         return fetch(fetchRequest).then(response => {
           // Check if valid response
-          if(!response || response.status !== 200 || response.type !== 'basic') {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
             return response;
           }
-          
+
           // Clone the response
           const responseToCache = response.clone();
-          
+
           caches.open(CACHE_NAME)
             .then(cache => {
               cache.put(event.request, responseToCache);
             });
-          
+
           return response;
         });
       })
